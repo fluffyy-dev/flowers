bc36870623dab1d2e5233730fbb0efd831d5f4f5
fluffy <cxkisner@gmail.com>
2026-07-21 19:36:57 +0000

    flower now appears :D

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Application } from "pixi.js";
+import { Application, Graphics } from "pixi.js";
 
 (async () => {
 
@@ -9,12 +9,44 @@ import { Application } from "pixi.js";
         // height: window.innerHeight,
 
         resizeTo: window,
-});
+    });
 
     app.canvas.style.position = 'absolute';
 
     // first flower
+    const flowerY = 300;
+    const flowerX = 300;
+    const petalWidth = 30;
+    const petalHeight = 50;
+    const carpelRad = 38;
+    const petals = 8;
+
+    const TurnDegrees = (2 * Math.PI) / petals;
+
+    for (let i = 0; i < petals; i++) {
+        const petal = new Graphics()
+        .ellipse(0, 0, petalWidth, petalHeight)
+        .fill({color: 0xffea00})
+        app.stage.addChild(petal);
+        petal.x = flowerX;
+        petal.y = flowerY;
+        petal.pivot.set(0, petalHeight);
+        petal.rotation = TurnDegrees * i;
+    }
+
+    const carpel = new Graphics()
+    .circle(300, 300, carpelRad)
+    .fill({color: 0x381607})
+    app.stage.addChild(carpel);
+
+    // center of flower
+    // const pete = new Graphics()
+    // .rect(300, 300, 6, 6)
+    // .fill({color: 0x42f59e})
+    // app.stage.addChild(pete);
+    // pete.pivot.set(3, 3);
+
+    // app.stage.addChild(petal1);
 
     document.body.appendChild(app.canvas)
-
 })();
